2d50e009884f79a0cdc18e59986ec9929dfc8bcc
Mark Roberts <marrober@redhat.com>
2020-04-21 15:06:34 +0100

    changes

--- copy.sh
+++ copy.sh
@@ -1,7 +1,4 @@
-oc new-app --image-stream=$1 --name=$2
-oc set env dc/$2 THIS_LAYER_NAME=$2 
-oc set env dc/$2 NEXT_LAYER_NAME=$5
-oc set env dc/$2 VERSION_ID=$3
-oc label dc/$2 app.kubernetes.io/part-of=$4 
+oc new-app --image-stream=$1 --name=$2 THIS_LAYER_NAME=$2 VERSION_ID=$3 STUDENT_NAME=$4 PORT=$5 NEXT_LAYER_NAME=$7
+oc label dc/$2 app.kubernetes.io/part-of=$6
 oc label dc/$2 app.openshift.io/runtime=nodejs
-oc expose service $2 --name=tcp-8080
+oc expose service $2
--- create.sh
+++ create.sh
@@ -1,9 +1,4 @@
-oc new-app nodejs~https://github.com/marrober/layers.git --name=$1 
-oc set env dc/$1 THIS_LAYER_NAME=$1 
-oc set env dc/$1 NEXT_LAYER_NAME=$4
-oc set env dc/$1 VERSION_ID=$2
-oc label dc/$1 app.kubernetes.io/part-of=$3
+oc new-app nodejs~https://github.com/marrober/layers.git --name=$1 THIS_LAYER_NAME=$1 VERSION_ID=$2 STUDENT_NAME=$3 PORT=$4 NEXT_LAYER_NAME=$6
+oc label dc/$1 app.kubernetes.io/part-of=$5
 oc label dc/$1 app.openshift.io/runtime=nodejs
-oc expose service $1 --name=tcp-8080
-
-
+oc expose service $1
