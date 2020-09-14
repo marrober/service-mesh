9570ad8387f700d304571eba858caff9abb5baa3
Mark Roberts <marrober@redhat.com>
2020-09-14 09:05:59 +0100

    Update

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
-oc new-app nodejs~https://github.com/marrober/layers.git --name=$1 THIS_LAYER_NAME=$1 VERSION_ID=$2 STUDENT_NAME=$3 PORT=$4 NEXT_LAYER_NAME=$6
-oc label dc/$1 app.kubernetes.io/part-of=$5
+oc new-app nodejs~https://github.com/marrober/layers.git --name=$1 THIS_LAYER_NAME=$1 VERSION_ID=$2 NEXT_LAYER_NAME=$4
+oc label dc/$1 app.kubernetes.io/part-of=$3
 oc label dc/$1 app.openshift.io/runtime=nodejs
 oc expose service $1
