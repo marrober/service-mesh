64ed2a65b0cd54fba86aae8e18e38faedc4b701c
Mark Roberts <marrober@redhat.com>
2021-05-12 17:06:53 +0100

    Update

--- network-policy/asset-creation.sh
+++ network-policy/asset-creation.sh
@@ -1,21 +1,22 @@
-oc new-project layers-left
-oc label namespace layers-left projectName=layers-left
+oc new-project left
+oc label namespace left projectName=left
 
-oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=left-01 --env=NEXT_LAYER_NAME=left-02 --env=NEXT_LAYER_NAMESPACE=layers-left --env=THIS_LAYER_NAME=left-01 --env=VERSION_ID=v1 --namespace=layers-left
+oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=liam --env=NEXT_LAYER_NAME=mark --env=NEXT_LAYER_NAMESPACE=left --env=THIS_LAYER_NAME=liam --env=VERSION_ID=v1 --namespace=left
 
-oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=left-02 --env=NEXT_LAYER_NAME=right-01 --env=NEXT_LAYER_NAMESPACE=layers-right --env=THIS_LAYER_NAME=left-02 --env=VERSION_ID=v1 --namespace=layers-left
+oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=mark --env=NEXT_LAYER_NAME=richard --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=mark --env=VERSION_ID=v1 --namespace=left
 
-oc expose svc/left-01 --namespace layers-left
+oc expose svc/liam --namespace left
 
-oc new-project layers-right
-oc label namespace layers-right projectName=layers-right
+oc new-project right
+oc label namespace right projectName=right
 
-oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=right-01 --env=NEXT_LAYER_NAME=right-02 --env=NEXT_LAYER_NAMESPACE=layers-right --env=THIS_LAYER_NAME=right-01 --env=VERSION_ID=v1 --namespace=layers-right
+oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=richard --env=NEXT_LAYER_NAME=stuart --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=richard --env=VERSION_ID=v1 --namespace=right
 
-oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=right-02 --env=THIS_LAYER_NAME=right-02 --env=VERSION_ID=v1 --namespace=layers-right
+oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=stuart --env=THIS_LAYER_NAME=stuart --env=VERSION_ID=v1 --namespace=right
 
-oc new-project layers-rogue
+oc new-project rogue
+oc label namespace rogue projectName=rogue
 
-oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=rogue --env=NEXT_LAYER_NAME=right-01 --env=NEXT_LAYER_NAMESPACE=layers-right --env=THIS_LAYER_NAME=rogue --env=VERSION_ID=v1 --namespace=layers-rogue
+oc new-app nodejs~https://github.com/marrober/layers.git#cross-project --name=rogue --env=NEXT_LAYER_NAME=richard --env=NEXT_LAYER_NAMESPACE=right --env=THIS_LAYER_NAME=rogue --env=VERSION_ID=v1 --namespace=rogue
 
-oc expose svc/rogue --namespace layers-rogue
+oc expose svc/rogue --namespace rogue
