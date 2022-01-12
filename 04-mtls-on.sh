264795d6e7d2bb003a99dd9d79c77ce73f38a3f4
Mark Roberts <marrober@redhat.com>
2022-01-12 08:52:50 +0000

    Update to mTLS

--- 04-mtls-on.sh
+++ 04-mtls-on.sh
@@ -3,3 +3,5 @@ oc create --save-config -f peer-authentication-mtls.yaml
 oc delete dr --all
 oc create --save-config -f destinationrule-mtls.yaml
 cd ..
+oc apply -f 01-simple-virtual-service/virtual-service-layer1.yaml
+oc apply -f 01-simple-virtual-service/virtual-service-layer2.yaml
