b277dd2a7d77ec6375f84d08ebdbc3c690396d5f
Mark Roberts <marrober@redhat.com>
2022-03-25 17:34:59 +0000

    Additions and changes

--- 06-retries-prep.sh
+++ 06-retries-prep.sh
@@ -7,4 +7,6 @@ oc apply -f virtual-service-layer1.yaml
 cd ../06-retries
 oc apply -f destination-rule-layer2.yaml
 oc apply -f virtual-service-layer2-initial.yaml
+oc create -f layer2-route.yaml
+oc get route/layer2 -o jsonpath='{"http://"}{.spec.host}{"\n\n"}'
 cd ..
