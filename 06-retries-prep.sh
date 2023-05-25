fc31bddbe504bf44fd9c904c7b20926154de18c8
Mark Roberts <marrober@redhat.com>
2023-05-25 16:46:21 +0100

    Updates and fixes

--- 06-retries-prep.sh
+++ 06-retries-prep.sh
@@ -8,6 +8,6 @@ oc apply -f virtual-service-layer1.yaml
 cd ../06-retries
 oc apply -f destination-rule-layer2.yaml
 oc apply -f virtual-service-layer2-initial.yaml
-oc create -f layer2-route.yaml
-oc get route/layer2 -o jsonpath='{"http://"}{.spec.host}{"/skip-on\n\n"}'
+# oc create -f layer2-route.yaml
+# oc get route/layer2 -o jsonpath='{"http://"}{.spec.host}{"/skip-on\n\n"}'
 cd ..
--- 09-trace-layers.sh
+++ 09-trace-layers.sh
@@ -1,9 +1,15 @@
 cd 09-trace-layers
+oc apply -f layer1.yaml
+oc apply -f layer2.yaml
 oc apply -f layer3.yaml
 oc apply -f layer4.yaml
 oc apply -f layer5.yaml
 oc apply -f layer6.yaml
 oc apply -f layer7.yaml
+oc delete destinationRule --all
+oc delete virtualService --all
+oc apply -f virtual-service-layer-1.yaml
+oc apply -f virtual-service-layer-2.yaml
 oc apply -f virtual-service-layer-3.yaml
 oc apply -f virtual-service-layer-4.yaml
 oc apply -f virtual-service-layer-5.yaml
